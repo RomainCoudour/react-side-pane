31417f7eee3437dd5ad1fd107b5fd8224cb16c59
Romain <rom.coudour@gmail.com>
2021-03-14 21:25:30 +0100

    chore(): init repo

---
+++ .babelrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+	presets: [["babel-preset-jason", { runtime: false }]],
+	plugins: [["babel-plugin-transform-react-remove-prop-types", { mode: "wrap" }]],
+	env: {
+		esm: {
+			presets: [["babel-preset-jason", { modules: false }]],
+		},
+	},
+};
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # react-side-panel
+
 React side panel
+
+[![code style: prettier](https://img.shields.io/badge/code_style-prettier-ff69b4.svg?style=flat-square)](https://github.com/prettier/prettier)
